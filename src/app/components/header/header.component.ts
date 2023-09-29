e6b5421e961bf6d5d93c47a9d93b871785dc2907
antoniosalinas2000 <salinas.antonio1820@gmail.com>
2023-09-28 23:40:44 -0500

    responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-
+import { provideIcons } from '@ng-icons/core';
+import { heroBars3 } from '@ng-icons/heroicons/outline';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  providers: [provideIcons({ heroBars3 })],
 })
 export class HeaderComponent {
+  openDropdown: boolean = false;
 
+  setDropdown() {
+    this.openDropdown = !this.openDropdown;
+  }
+
+  dropdownOptions = [ 'Inicio', 'Eventos', 'Foro'];
+  
 }
