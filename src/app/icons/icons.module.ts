416fc031752a3208ca2db4dea740ce886e92eab1
Joseph Ramirez Sarmiento <U20211C828@upc.edu.pe>
2023-11-23 06:29:31 -0500

    adding bootstrap icons to dependicies

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
 import { heroCalendar } from '@ng-icons/heroicons/outline';
 import { heroBars3 } from '@ng-icons/heroicons/outline';
+import { bootstrapFacebook } from '@ng-icons/bootstrap-icons';
+import { bootstrapInstagram } from '@ng-icons/bootstrap-icons';
+import { bootstrapTwitter } from '@ng-icons/bootstrap-icons';
 
 import { heroCalendarSolid } from '@ng-icons/heroicons/solid';
 @NgModule({
@@ -13,6 +16,9 @@ import { heroCalendarSolid } from '@ng-icons/heroicons/solid';
       heroCalendar: heroCalendar,
       heroBars3: heroBars3,
       heroCalendarSolid: heroCalendarSolid,
+      bootstrapFacebook: bootstrapFacebook,
+      bootstrapInstagram: bootstrapInstagram,
+      bootstrapTwitter: bootstrapTwitter,
     }),
   ],
 })
