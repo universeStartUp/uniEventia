4eb405488fc5e035b43fc50d670a0c47872e9de6
antoniosalinas2000 <salinas.antonio1820@gmail.com>
2023-11-24 17:23:10 -0500

    redirects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dev--animated-alfajores-2b8bf3.netlify.app/api',
+  apiUrl: 'https://animated-alfajores-2b8bf3.netlify.app/api',
   apiKey:
     'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiU1RVREVOVCIsInN1YiI6InUyMDIxMWM4MjhAdXBjLmVkdS5wZSIsImlhdCI6MTcwMDcyODI4NywiZXhwIjoxNzAwNzI5NzI3fQ.L59zltXAUXFSoDHHgGU58WRYkKN4_yaSQGM2lrooGtY',
 };
