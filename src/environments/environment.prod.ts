889f64251bed9f9d2970554161dd5b8455a47886
antoniosalinas2000 <salinas.antonio1820@gmail.com>
2023-11-24 16:51:12 -0500

    api configuration redirects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.128.205.27',
+  apiUrl: 'https://dev--animated-alfajores-2b8bf3.netlify.app/api',
   apiKey:
     'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiU1RVREVOVCIsInN1YiI6InUyMDIxMWM4MjhAdXBjLmVkdS5wZSIsImlhdCI6MTcwMDcyODI4NywiZXhwIjoxNzAwNzI5NzI3fQ.L59zltXAUXFSoDHHgGU58WRYkKN4_yaSQGM2lrooGtY',
 };
